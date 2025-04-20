8c72fd13330979139b126cabc7a613719345fbbd
cjwattttts <cameron.jwatts@outlook.com>
2025-04-20 11:26:15 -0400

    Reuse TourCard with read more toggle and removal

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,12 +1,22 @@
-<article>
-  <img src={image} alt={name} />
-  <h2>{name}</h2>
-  <h4>${price}</h4>
-  <p>
-    {readMore ? info : `${info.substring(0, 100)}... `}
-    <button onClick={() => setReadMore(!readMore)}>
-      {readMore ? 'Show Less' : 'Read More'}
-    </button>
-  </p>
-  <button onClick={() => onRemove(id)}>Not Interested</button>
-</article>
+import React, { useState } from 'react';
+
+const TourCard = ({ id, name, info, image, price, onRemove }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <article>
+      <img src={image} alt={name} />
+      <h2>{name}</h2>
+      <h4>${price}</h4>
+      <p>
+        {readMore ? info : `${info.substring(0, 100)}... `}
+        <button onClick={() => setReadMore(!readMore)}>
+          {readMore ? 'Show Less' : 'Read More'}
+        </button>
+      </p>
+      <button onClick={() => onRemove(id)}>Not Interested</button>
+    </article>
+  );
+};
+
+export default TourCard;
