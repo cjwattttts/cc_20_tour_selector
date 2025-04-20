81d875692f483465426d55c81a4fd234118e3c35
cjwattttts <cameron.jwatts@outlook.com>
2025-04-20 11:11:15 -0400

    Setup root component with state and props structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Gallery from './components/gallery.jsx';
+
+const App = () => {
+  return (
+    <main>
+      <Gallery />
+    </main>
+  );
+};
+
+export default App;
